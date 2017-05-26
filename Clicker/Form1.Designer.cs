df3f585ad0fb20bde5cac8e19b787d6abeff3841
terra-mich <pomeasa32@gmail.com>
2017-05-26 23:44:52 +0900

    1st commmit
    1st publish

---
+++ Clicker/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace Clicker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.keyboardChecker = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.clicker = new System.Windows.Forms.Timer(this.components);
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // keyboardChecker
+            // 
+            this.keyboardChecker.Enabled = true;
+            this.keyboardChecker.Interval = 1;
+            this.keyboardChecker.Tick += new System.EventHandler(this.keyboardChecker_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "状態：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(45, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "無効";
+            // 
+            // clicker
+            // 
+            this.clicker.Enabled = true;
+            this.clicker.Interval = 2;
+            this.clicker.Tick += new System.EventHandler(this.clicker_Tick);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(15, 34);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(59, 19);
+            this.numericUpDown1.TabIndex = 2;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(80, 36);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(20, 12);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "ms";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("MS UI Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label4.Location = new System.Drawing.Point(124, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(120, 15);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "LeftCtrl+F7　開始";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("MS UI Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label5.Location = new System.Drawing.Point(124, 36);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(120, 15);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "LeftCtrl+F8　停止";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(281, 65);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "左クリック連打ツール(仮)";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer keyboardChecker;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer clicker;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Clicker/Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows.Forms;
+using System.Windows.Input;
+
+namespace Clicker
+{
+    /*
+     * TODO: キーコンフィグ出来るようになるといいな
+     * WIN32APIを結構使っちゃってるからPureじゃない気がするけどしかたないね
+     */
+    public partial class Form1 : Form
+    {
+        [StructLayout(LayoutKind.Sequential, Pack = 4)] //WIN32APIでいうところのLPRECT構造体にあたる構造体
+        private struct RECT
+        {
+            public int left;
+            public int top;
+            public int right;
+            public int bottom;
+        }
+        [DllImport("User32.Dll")]
+        static extern int GetWindowRect(IntPtr hWnd, out RECT lpRect); //ウィンドウハンドラを渡すとウィンドウの座標を返してくれるAPIだよ
+        [DllImport("User32.dll")]
+        static extern int SetCursorPos(int x, int y); //カーソル位置を設定する感じかも
+        [DllImport("USER32.dll")]
+        static extern void mouse_event(int dwFlags, int dx, int dy, int cButtons, int dwExtraInfo); //マウスのイベントを起こす 色々イベントはあるらしいけどUPとDOWNしか使わないね
+
+        //定数
+        private const int MOUSEEVENTF_LEFTDOWN = 0x2;
+        private const int MOUSEEVENTF_LEFTUP = 0x4;
+        private const int MARGIN = 6;
+
+        private bool isExecuting = false; //現在実行中かを保持しておく変数だね
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private static void LeftClick()
+        {
+            mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0); //押してー
+            Thread.Sleep(1);
+            mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0); //離す！
+        }
+
+        public static Process GetActiveProcess()
+        {
+            // アクティブなウィンドウハンドルの取得
+            IntPtr hWnd = WinAPI.GetForegroundWindow();
+            int id;
+            // ウィンドウハンドルからプロセスIDを取得
+            WinAPI.GetWindowThreadProcessId(hWnd, out id);
+            Process process = Process.GetProcessById(id);
+            return process;
+        }
+
+        private void keyboardChecker_Tick(object sender, EventArgs e)
+        {
+            label2.Text = isExecuting ? "有効" : "無効";
+            if(isExecuting)
+            {
+                if(Keyboard.IsKeyDown(Key.F8) && Keyboard.IsKeyDown(Key.LeftCtrl))
+                {
+                    isExecuting = false;
+                    return;
+                }
+                Process p = GetActiveProcess();
+                if(p.MainWindowHandle != IntPtr.Zero) //ウィンドウハンドラ持ってるかどうか判定する(ウィンドウから取得したプロセスだから持ってるとは思うけど一応)
+                {
+                    Point mousePos = MousePosition;
+                    RECT windowRect = new RECT();
+
+                    GetWindowRect(p.MainWindowHandle, out windowRect); //アクティブウィンドウの座標を取得
+
+                    int mouseX = mousePos.X, mouseY = mousePos.Y;
+
+                    bool flag = false; //カーソルを移動するか判定する変数だよ(これを入れないとガクガクになるよ)
+
+
+                    //これ以降はマウスの動きを制限するコードだよ
+                    if(mouseX < windowRect.left + MARGIN)
+                    {
+                        flag = true;
+                        mouseX = windowRect.left + MARGIN;
+                    }
+                    if(mouseX > windowRect.right - MARGIN)
+                    {
+                        flag = true;
+                        mouseX = windowRect.right - MARGIN;
+                    }
+                    if(mouseY < windowRect.top + 4)
+                    {
+                        flag = true;
+                        mouseY = windowRect.top + MARGIN;
+                    }
+                    if(mouseY > windowRect.bottom - MARGIN)
+                    {
+                        flag = true;
+                        mouseY = windowRect.bottom - MARGIN;
+                    }
+                    //Console.WriteLine("mouseX':{0}, mouseY':{1}", mouseX, mouseY);
+                    if (flag)
+                    {
+                        SetCursorPos(mouseX, mouseY);
+                    }
+                }
+            }
+            else
+            {
+                if (Keyboard.IsKeyDown(Key.F7) && Keyboard.IsKeyDown(Key.LeftCtrl))
+                {
+                    Console.WriteLine("Enabled");
+                    isExecuting = true;
+                    return;
+                }
+            }
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            clicker.Interval = (int)numericUpDown1.Value;
+        }
+
+        private void clicker_Tick(object sender, EventArgs e)
+        {
+            if(isExecuting) //実行してるかな？
+            {
+                LeftClick(); //クリック！
+            }
+        }
+    }
+    public class WinAPI
+    {
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetForegroundWindow(); //現在アクティブなウィンドウハンドラを返すよ
+
+        [DllImport("user32.dll")]
+        public static extern int GetWindowThreadProcessId(IntPtr hWnd, out int lpdwProcessId); //ウィンドウハンドラからプロセスIDを割り出すよ
+    }
+}
